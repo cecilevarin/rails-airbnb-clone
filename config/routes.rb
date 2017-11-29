312e669d23c192c90126349e551779c7dbc951af
Victor Duperret <v.duperret@gmail.com>
2017-11-29 15:35:46 +0100

    Controller à tester en cours

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'gears#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :gears
+  resources :gears do
+    resources :bookings, only: [:new, :create, :destroy, :edit]
+  end
 end
 
